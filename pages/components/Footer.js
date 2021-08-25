13348d8620bd2ddadd1e7f2a25af9b1c3a1253f0
baneofmorpheus <epicgenii18@gmail.com>
2021-08-25 13:56:42 +0100

    bts

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
 const Footer = () => {
-    return ( 
-        <div className="footer">
-            <h1>Footer.exe</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div className="footer">
+      <div className="backtotop">
+        <Link href="/">
+          <a>Back To Top</a>
+        </Link>
+      </div>
+      <h1>Footer.exe</h1>
+    </div>
+  );
+};
+
 export default Footer;
