db92651f3327280683069cbaad31cbce81fec359
Michael Okwuosah <87485797+m1chae-l@users.noreply.github.com>
2021-08-31 15:25:21 +0100

    Update Layout.js

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,11 +1,13 @@
-import BottomRightSideBar from "./BottomRightSideBar";
+import { useState, useEffect } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }, props) => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="layoutcomponent">
-      <Navbar />
+      <Navbar search={search} setSearch={setSearch}/>
       {children}
       <Footer />
     </div>
