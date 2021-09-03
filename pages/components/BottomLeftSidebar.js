1f91a644882761b0d1ce17af9fb377f9378c61c0
Michael Okwuosah <87485797+m1chae-l@users.noreply.github.com>
2021-09-03 15:31:19 +0100

    Add files via upload

---
+++ pages/components/BottomLeftSidebar.js
@@ -0,0 +1,10 @@
+const BottomLeftSideBar = () => {
+    return (
+        <div className="browse">
+            <h2>Browse</h2>
+            <p>Edit This Later (Glossary)</p>
+        </div>
+    );
+}
+
+export default BottomLeftSideBar;
